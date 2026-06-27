3e9fe287b0128e2eb0f2199ef2df1d235eb315a0
erFrancaa <erfranca.com@gmail.com>
2026-06-27 14:23:11 -0300

    implementando sidebar no app.vue

--- src/App.vue
+++ src/App.vue
@@ -11,31 +11,54 @@ const auth = useAuthStore()
 
 <template>
 
-<nav class="nav">
+
+<div class="layout">
 
 
-<div class="links">
+<aside class="sidebar">
+
+
+<h2 class="logo">
+✓ Tasks
+</h2>
+
+
+
+<nav>
 
 
 <router-link to="/">
-Tasks
+
+📝 Tarefas
+
 </router-link>
+
 
 
 <router-link to="/dashboard">
-Dashboard
+
+📊 Dashboard
+
 </router-link>
 
 
-</div>
+
+</nav>
 
 
-<div 
-v-if="auth.logado"
-class="user"
->
 
+<div class="profile" v-if="auth.logado">
+
+
+<div>
+
+Olá,
+
+<strong>
 {{ auth.usuario }}
+</strong>
+
+</div>
 
 
 <button @click="auth.logout">
@@ -48,42 +71,136 @@ Sair
 </div>
 
 
-</nav>
+
+</aside>
+
+
+
+
+<main class="content">
 
 
 <router-view />
 
 
+</main>
+
+
+
+</div>
+
+
 </template>
 
-<style scoped>   
-.nav{
 
+
+<style scoped>
+
+
+.layout{
 display:flex;
-justify-content:space-between;
-align-items:center;
-padding:16px 22px;
-background:rgba(255,255,255,.7);
-backdrop-filter:blur(20px);
-border-radius:20px;
-border:1px solid rgba(0,0,0,.05);
-box-shadow:0 10px 30px rgba(0,0,0,.08);
-margin-bottom:40px;
+min-height:100vh;
+gap:25px;
 }
 
-.links a{
 
-margin-right:25px;
+
+
+.sidebar{
+width:260px;
+height:calc(100vh - 80px);
+position:sticky;
+top:40px;
+background:rgba(255,255,255,.65);
+backdrop-filter:blur(25px);
+border-radius:25px;
+padding:25px;
+border:1px solid rgba(0,0,0,.06);
+box-shadow:0 15px 40px rgba(0,0,0,.08);
+
+
+}
+
+
+
+.logo{
+margin-bottom:35px;
+font-size:22px;
+}
+
+
+
+nav{
+display:flex;
+flex-direction:column;
+gap:10px;
+
+}
+
+
+
+nav a{
+padding:12px 15px;
+border-radius:12px;
 text-decoration:none;
 color:#555;
-font-weight:500;
+transition:.2s;
+
+
 }
 
 
 
-.links a.router-link-active{
-color:#000;
+nav a:hover{
+background:#f0f0f5;
+
 
 }
+
+
+
+nav a.router-link-active{
+background:#007aff;
+color:white;
+
+}
+
+
+
+.content{
+flex:1;
+padding:20px;
+
+
+}
+
+.profile{
+
+position:absolute;
+bottom:25px;
+left:25px;
+right:25px;
+background:white;
+padding:15px;
+border-radius:15px;
+
+
+}
+
+
+
+button{
+margin-top:10px;
+width:100%;
+background:#007aff;
+color:white;
+border:none;
+padding:10px;
+border-radius:20px;
+cursor:pointer;
+
+}
+
+
 
 </style>
